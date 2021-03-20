cacb2d5469312fcb5fcaa74c0c799edb4a14e2d8
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-03-20 14:51:46 -0300

    feat:added router

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { GlobalStyle } from './styles/global';
 import themeColor from './styles/themeDark';
 
@@ -8,7 +9,13 @@ export const App: React.FC = () => {
   return (
     <MuiThemeProvider theme={themeColor}>
       <ThemeProvider theme={themeColor}>
-        <div>Hello</div>
+        <Router>
+          <Switch>
+            <Route {...home}>
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
         <GlobalStyle />
       </ThemeProvider>
     </MuiThemeProvider>
