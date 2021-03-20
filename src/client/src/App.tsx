690b933cd2dcf2d4637dcae19bd9064026b47caa
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-03-20 14:30:45 -0300

    feat:theme added to client

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { GlobalStyle } from './styles/global';
+import themeColor from './styles/themeDark';
 
-export const App: React.FC = () => (
-  <>
-    <h1>Hello</h1>
-    <GlobalStyle />
-  </>
-);
+export const App: React.FC = () => {
+  return (
+    <MuiThemeProvider theme={themeColor}>
+      <ThemeProvider theme={themeColor}>
+        <div>Hello</div>
+        <GlobalStyle />
+      </ThemeProvider>
+    </MuiThemeProvider>
+  );
+};
